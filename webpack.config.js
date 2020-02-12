81feda6a6cefdc82e4f6c77db414eff4235b7c88
jaredkc <jaredkc@gmail.com>
2020-02-12 15:35:57 -0700

    Adding babel to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,17 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      },
+      {
         test: /\.scss$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
