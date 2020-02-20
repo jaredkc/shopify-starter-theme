e8e912d95c5ebb61fe1268b2d38d6a4fd629c06a
jaredkc <jaredkc@gmail.com>
2020-02-20 11:36:43 -0700

    Hookup lazysizes for responsive images

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,6 +1,10 @@
 /**
  * Scripts and styles used globally
  */
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+import 'lazysizes/plugins/rias/ls.rias';
+import 'lazysizes';
+
 import '../scss/theme.scss';
 
 console.log('Hello from index.js');
