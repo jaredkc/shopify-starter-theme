896b629dcb48503fdf1fef9a4cf94b5945744cc9
jaredkc <jaredkc@gmail.com>
2020-04-10 16:07:09 -0600

    Adding cssnano to minify

--- postcss.config.js
+++ postcss.config.js
@@ -14,6 +14,7 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    require('cssnano')({ preset: 'default' }),
     ...process.env.NODE_ENV === 'production'
       ? [purgecss]
       : [],
