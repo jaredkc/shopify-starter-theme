3dd5f350008e601ebbbe6a4aeb78128911cb31ff
jaredkc <jaredkc@gmail.com>
2020-04-15 11:53:02 -0600

    Linting fixes for postcss.config

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,22 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const tailwindcss = require('tailwindcss');
 
+const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
   content: [
     './theme/**/*.liquid',
     './src/**/*.svelte',
   ],
-
   // Include any special characters you're using in this regular expression
   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
 });
 
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    require('cssnano')({ preset: 'default' }),
+    tailwindcss(),
+    autoprefixer(),
+    cssnano({ preset: 'default' }),
     ...process.env.NODE_ENV === 'production'
       ? [purgecss]
       : [],
