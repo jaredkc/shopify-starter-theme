87559e67c82c83a569fe4924e720d7cd27b350cb
jaredkc <jaredkc@gmail.com>
2020-05-14 19:41:55 -0600

    Hookup the product form

--- src/js/product.js
+++ src/js/product.js
@@ -1,47 +1,102 @@
 /**
  * Product specific scripts and styles
  */
+import { ProductForm } from '@shopify/theme-product-form';
+import { addItem } from '@shopify/theme-cart';
+
 import '../css/product.css';
 import './utility/public-path';
 import openCart from './utility/open-cart';
 
-/**
- * Ajax add to cart,
- * then open cart module
- */
-// TODO handle file uploads
-function submitToCart(e) {
-  e.preventDefault();
+const addToCartBtn = document.querySelector('[data-add-to-cart]');
+const featuredImage = document.querySelector('[data-featured-image]');
+const formElement = document.querySelector('[data-product-form]');
+const stockMessages = document.querySelectorAll('[data-stock-message]');
+const thumbnailLinks = document.querySelectorAll('[data-thumbnail-links]');
 
-  const btn = document.getElementById('AddToCart');
-  const loadingClass = 'loading';
-
-  btn.classList.add(loadingClass);
-
-  fetch(e.target.action, {
-    method: 'POST',
-    body: new URLSearchParams(new FormData(e.target)),
-  }).then((res) => {
-    btn.classList.remove(loadingClass);
-
-    if (res.status === 200) {
-      openCart().then((mRes) => {
-        if (mRes === false) {
-          window.location.href = '/cart';
-        }
-      });
-    } else {
-      // TODO handle bad requests
-      alert('Oh no! There was an problem.');
-    }
-  }).catch((error) => {
-    btn.classList.remove(loadingClass);
-    // TODO handle error
-    console.log(error);
-  });
+// Update the featured image
+function handleFeaturedImage(imgSrc, imgAltText) {
+  featuredImage.src = imgSrc;
+  featuredImage.alt = imgAltText;
+  // Unset srcset as it overrides src
+  featuredImage.srcset = '';
 }
 
-const addToCartForm = document.getElementById('AddToCartForm');
-if (addToCartForm) {
-  addToCartForm.addEventListener('submit', submitToCart);
+// ProductForm callback to handle variants selection changes
+function onOptionChange(event) {
+  const { variant } = event.dataset;
+
+  // Hide all stock message, then show for this variant
+  stockMessages.forEach((stockMessage) => stockMessage.classList.add('hidden'));
+  document.getElementById(`stock-message-${variant.id}`).classList.remove('hidden');
+
+  // Update feature image
+  if (variant.featured_image) {
+    handleFeaturedImage(variant.featured_image.src, variant.featured_image.alt);
+  }
+
+  if (variant === null) {
+    // The combination of selected options does not have a matching variant
+    addToCartBtn.disabled = true;
+  } else if (variant && !variant.available) {
+    // The combination of selected options has a matching variant but it is currently unavailable
+    addToCartBtn.disabled = true;
+  } else if (variant && variant.available) {
+    // The combination of selected options has a matching variant and it is available
+    addToCartBtn.disabled = false;
+  }
+}
+
+// ProductForm callback to handle AJAX form submit.
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  addToCartBtn.classList.add('loading');
+
+  const { id } = event.dataset.variant;
+  const { quantity } = event.dataset;
+  const { properties } = event.dataset;
+
+  addItem(id, { quantity, properties })
+    .then(() => {
+      addToCartBtn.classList.remove('loading');
+      openCart().then((response) => {
+        if (response === false) { window.location.href = '/cart'; }
+      });
+    })
+    .catch(() => {
+      addToCartBtn.classList.remove('loading');
+      // Minimal error messages, so submit form
+      // for server and template logic as fallback.
+      formElement.submit();
+    });
+}
+
+// Additional callbacks available
+// function onQuantityChange(event) {}
+// function onPropertyChange(event) {}
+
+// Fetch the product data from the .js endpoint because it includes
+// more data than the .json endpoint. Alternatively, you could inline the output
+// of {{ product | json }} inside a <script> tag, with the downside that the
+// data can never be cached by the browser.
+//
+// You will need to polyfill `fetch()` if you want to support IE11
+fetch(`/products/${formElement.dataset.productHandle}.js`)
+  .then((response) => response.json())
+  .then((productJSON) => {
+    const productForm = new ProductForm(formElement, productJSON, {
+      onOptionChange, onFormSubmit,
+    });
+  });
+
+
+// Update featured image when you click on thumbnails
+if (thumbnailLinks) {
+  thumbnailLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      handleFeaturedImage(event.currentTarget.href, event.target.alt);
+    });
+  });
 }
