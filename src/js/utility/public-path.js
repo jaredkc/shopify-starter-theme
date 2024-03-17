f3df085125b90703b1e9f276e3eb577bd2b92e04
jaredkc <jaredkc@gmail.com>
2024-03-16 21:12:49 -0600

    CSS migration

--- src/js/utility/public-path.js
+++
@@ -1,5 +0,0 @@
-/**
- * Required for lazy-loading with Webpack on Shopify CDN
- * Generated in theme.liquid, referenced here and imported in theme.js
- */
-__webpack_public_path__ = window.__webpack_public_path__;
