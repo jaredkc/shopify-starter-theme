3b8aa91c652032520525e48b7ffb2f1f6975b6a2
jaredkc <jaredkc@gmail.com>
2024-03-16 21:19:24 -0600

    Remove JS that will not be used

--- src/js/utility/open-cart.js
+++
@@ -1,7 +0,0 @@
-const openCart = () => import(/* webpackChunkName: "app-cart-init" */ '../apps/app-cart-init').then((module) => {
-  const appcart = module.default;
-  appcart();
-  return true;
-}).catch(() => false);
-
-export default openCart;
