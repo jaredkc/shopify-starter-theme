12494c597070a1b4d4dcafdb9c8f1bb02f58463c
jaredkc <jared@clivecoffee.com>
2024-06-11 16:09:00 -0600

    Basic search dialog modal

--- assets/global.js
+++ assets/global.js
@@ -1,2 +1,20 @@
 // JS is enabled
 document.querySelector('html').classList.add('js');
+
+//
+// Site search in dialog modal
+//
+const searchDialog = document.querySelector('.search-dialog');
+const openSearch = document.querySelectorAll('.open-search');
+
+openSearch.forEach((element) => {
+  element.addEventListener('click', () => {
+    searchDialog.showModal();
+  });
+});
+
+searchDialog.addEventListener('click', (event) => {
+  if (event.target === searchDialog) {
+    searchDialog.close();
+  }
+});
