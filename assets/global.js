724bfa2e342ae49fe0714dee9cac43f6af81fea2
jaredkc <jared@clivecoffee.com>
2024-06-11 21:24:16 -0600

    Sticky header on scroll up

--- assets/global.js
+++ assets/global.js
@@ -18,3 +18,61 @@ searchDialog.addEventListener('click', (event) => {
     searchDialog.close();
   }
 });
+
+/**
+ * StickyHeader
+ * Shows the header when scrolling up
+ */
+class StickyHeader extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.header = document.querySelector('.section-header');
+    this.height = this.header.offsetHeight;
+    this.currentScrollTop = 0;
+    this.stuckClass = 'sticky-header-stuck';
+    this.hiddenClass = 'sticky-header-hidden';
+    window.addEventListener('scroll', this.onScroll.bind(this));
+  }
+
+  disconnectedCallback() {
+    window.removeEventListener('scroll', this.onScroll.bind(this));
+  }
+
+  onScroll() {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollTop === 0) {
+      window.requestAnimationFrame(this.reset.bind(this));
+    }
+
+    if (scrollTop < this.height) return;
+
+    if (scrollTop < this.currentScrollTop) {
+      window.requestAnimationFrame(this.show.bind(this));
+    } else {
+      window.requestAnimationFrame(this.hide.bind(this));
+    }
+
+    this.currentScrollTop = scrollTop;
+  }
+
+  reset() {
+    this.header.classList.remove(this.stuckClass);
+    this.header.classList.remove(this.hiddenClass);
+  }
+
+  hide() {
+    this.header.classList.add(this.hiddenClass);
+    this.header.classList.remove(this.stuckClass);
+  }
+
+  show() {
+    this.header.classList.remove(this.hiddenClass);
+    this.header.classList.add(this.stuckClass);
+  }
+}
+
+customElements.define('sticky-header', StickyHeader);
